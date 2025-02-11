38c8ee7bdff01a11805cd00e5b94114baaff4f15
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-02-11 03:39:29 +0330

    js-site-template: add responsive style

--- js/app.js
+++ js/app.js
@@ -3,23 +3,34 @@ const themeToggleSidebar = document.getElementById('theme-toggle-sidebar');
 const hamburger = document.querySelector(".hamburger")
 const sidebar = document.querySelector(".sidebar")
 
+
 //todo : Dark/Light 
 themeToggle.addEventListener('click', () => {
    const currentTheme = document.body.classList.contains('dark') ? 'dark' : 'light';
    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
    document.body.classList.replace(currentTheme, newTheme);
    localStorage.setItem('theme', newTheme);
+
+   themeToggle.innerHTML = newTheme === "dark" ? "تم روشن" : "تم تاریک"
+   themeToggleSidebar.innerHTML = newTheme === "dark" ? "تم روشن" : "تم تاریک"
 });
 themeToggleSidebar.addEventListener('click', () => {
    const currentTheme = document.body.classList.contains('dark') ? 'dark' : 'light';
    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+   
    document.body.classList.replace(currentTheme, newTheme);
    localStorage.setItem('theme', newTheme);
+
+   themeToggle.innerHTML = newTheme === "dark" ? "تم روشن" : "تم تاریک"
+   themeToggleSidebar.innerHTML = newTheme === "dark" ? "تم روشن" : "تم تاریک"
 });
 
 // بارگذاری تم ذخیره‌شده
 window.onload = () => {
    const savedTheme = localStorage.getItem('theme') || 'light';
+   themeToggle.innerHTML = savedTheme === "dark" ? "تم روشن" : "تم تاریک"
+   themeToggleSidebar.innerHTML = savedTheme === "dark" ? "تم روشن" : "تم تاریک"
    document.body.classList.add(savedTheme);
 };
 //todo: Navbar Sidber
