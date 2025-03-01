2b27e58629974cefb768d3cb44969ea48220ba33
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-03-01 17:39:15 +0330

    add slider1 file project

---
+++ projects/3-components/06-sliders/01-slider1/js/app.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+    loop: true, // boolean
+    margin: 10, // Number
+    nav: true, // Boolean
+    dots: true, // Boolean
+    autoplay: true, // Boolean
+    autoplayTimeout: 2000, // Number
+    center: true, // Boolean
+    rtl: true, // Boolean
+    smartSpeed: 3000, // Boolean
+    items: 5, // Number
+
+})
