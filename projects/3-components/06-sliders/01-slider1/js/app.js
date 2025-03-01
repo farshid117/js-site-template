de25dbe04e8dea5fb11a05ac12aee579da0f3d1d
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-03-01 18:09:29 +0330

    some refacot slider1

--- projects/3-components/06-sliders/01-slider1/js/app.js
+++ projects/3-components/06-sliders/01-slider1/js/app.js
@@ -10,4 +10,24 @@ $('.owl-carousel').owlCarousel({
     smartSpeed: 3000, // Boolean
     items: 5, // Number
 
+    responsive: {
+        0: {
+            items: 1,
+            dots: false,
+            nav: false,
+        },
+        480: {
+            items: 2,
+            dots: true,
+            nav: false,
+        },
+        600: {
+            items: 3,
+            margin: 20,
+        },
+        1000: {
+            items: 4
+        }
+    }
+
 })
