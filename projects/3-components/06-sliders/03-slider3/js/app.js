9f8952b8120f8f527d7b2284fc65bc84756c33ad
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-03-05 14:06:52 +0330

    add flip slider to 03-slider3

--- projects/3-components/06-sliders/03-slider3/js/app.js
+++ projects/3-components/06-sliders/03-slider3/js/app.js
@@ -1,8 +1,17 @@
-    var swiper = new Swiper(".mySwiper", {
-            effect: "cards",
-            grabCursor: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-        });
+const swiper1 = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+const swiper2 = new Swiper('.swiper2', {
+    effect: 'flip', // انتخاب افکت ورق زدن
+    grabCursor: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
