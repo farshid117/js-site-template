354419a23693ec6d04f9b64ddce391b22ef3f756
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-03-16 00:23:55 +0330

    06-header6 : responsive2

--- projects/3-components/02-headers/06-header6/js/app.js
+++ projects/3-components/02-headers/06-header6/js/app.js
@@ -1,5 +1,9 @@
 const hamburBtn = document.querySelector(".hamburger")
+const navbarElm = document.querySelector(".navbar")
 
 hamburBtn.addEventListener("click", (event) => {
     hamburBtn.classList.toggle("show")
+})
+navbarElm.addEventListener("click", (event) => {
+    hamburBtn.classList.remove("show")
 })
