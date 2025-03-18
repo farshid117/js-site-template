e2868f0c938d598872a8ab167ff5cd28219032b1
alirez khayatpour <alireza_khayatpour@yahoo.com>
2025-03-18 17:33:35 +0330

    05-slider5 app.js edited

--- projects/3-components/06-sliders/05-slider5/js/app.js
+++ projects/3-components/06-sliders/05-slider5/js/app.js
@@ -1,16 +1,11 @@
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: false,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
+    zoom: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
     },
 });
